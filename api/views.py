afbec3af927b3f9b63172fa15bb6eaf9d5ee4a21
Abhinav Kumar <abhinav@outlook.com>
2017-07-29 08:11:50 +0530

    Book slot

--- api/views.py
+++ api/views.py
@@ -4,41 +4,40 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Sloats
-import json
-from datetime import date, datetime
+from datetime import date, datetime, time
 import json as simplejson
 
 # Create your views here.
 
 
-def book_slot(self):
-    return HttpResponse("Hei")
-
-
 def book_a_slot(request):
-    username = request.GET.getlist('username')
-    start_time = request.GET.get('starttime')
+    username = 'default'
+    d = request.GET.get('date')
+    d = datetime.strptime(d, '%Y-%m-%d')
+    t = request.GET.get('starttime')
+    t = datetime.strptime(t, '%I %p')
+    slot_time = datetime.combine(date(d.year, d.month, d.day), time(t.hour))
     booked_by_user = request.GET.get('bookedbyuser')
     # we added this so that we can have pet note type with '&' in between them
     # we replace the '&' with '_' from front-end so that it can be send
     # here we are converting it back to the previous(same) name
-    bookslot = Sloats(username= username, start_time = start_time,
+    bookslot = Sloats(username= username, start_time = slot_time,
              booked_by_user = booked_by_user)
     bookslot.save()
-    return HttpResponse("DATA SAVED SUCCESSFULLY")
+    return HttpResponse(simplejson.dumps({"status": "success"}))
 
 
 def find_a_slot(request):
-    start_time = request.GET.get('starttime')
+    date = request.GET.get('date')
     # we added this so that we can have pet note type with '&' in between them
     # we replace the '&' with '_' from front-end so that it can be send
     # here we are converting it back to the previous(same) name
     all_sloats = list(range(24))
-    start_date = datetime.strptime(start_time, "%Y-%m-%d")
+    start_date = datetime.strptime(date, "%Y-%m-%d")
     booked_sloats = Sloats.objects.filter(
         start_time__year=start_date.year,
         start_time__month=start_date.month,
         start_time__day=start_date.day
     ).only('start_time')
     free_sloats = list(set(all_sloats) - set(booked_sloats))
-    return HttpResponse(simplejson.dumps({"freesloats": free_sloats}))
+    return HttpResponse(simplejson.dumps({"status": "success", "freesloats": free_sloats}))
